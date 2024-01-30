f51cfab78657a349b29b76dd8efd00090a6ffc7c
Marcus Kitchen <marcusdkitchen@gmail.com>
2024-01-30 10:17:27 -0500

    routes and views lab

--- server/app.py
+++ server/app.py
@@ -4,5 +4,36 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:string>')
+def print_string(string):
+    print (string)
+    return f'{string}'
+
+@app.route('/count/<int:number>')
+def count(number):
+    output = ''
+    for i in range(number):
+        output += str(i) + "\n"
+    return output
+
+@app.route('/math/<int:num1>/<oper>/<int:num2>')
+def math(num1, oper, num2):
+    res = None
+    if oper == '+':
+        res=num1+num2
+    elif oper == '-':
+        res=num1-num2
+    elif oper == '*':
+        res=num1*num2
+    elif oper == 'div':
+        res=num1/num2
+    elif oper == '%':
+        res=num1%num2
+    return str(res)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
